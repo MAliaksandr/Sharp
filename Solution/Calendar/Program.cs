08b4e709f804dc443b5b84717cc0e3e67d0ed2dc
Aliaksandr <mosya911@gmail.com>
2022-05-29 14:33:07 +0300

    y

---
+++ Solution/Calendar/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Calendar // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. This application return name day of the week by number");
+            Console.WriteLine("Please, enter number of the day or 'EXIT' to exit.");
+
+            string str;
+            do
+            {
+                str = Console.ReadLine();
+
+                int D;
+                bool isInt = int.TryParse(str, out D);
+
+                string dayName;
+
+                if (isInt)
+                {
+
+                    switch (D)
+                    {
+                        case 1:
+                            dayName = "Mondey";
+                            break;
+                        case 2:
+                            dayName = "Tuesday";
+                            break;
+                        case 3:
+                            dayName = "Wednesday";
+                            break;
+                        case 4:
+                            dayName = "Thursday";
+                            break;
+                        case 5:
+                            dayName = "Friday";
+                            break;
+                        case 6:
+                            dayName = "Saturday";
+                            break;
+                        case 7:
+                            dayName = "Sunday";
+                            break;
+                        default:
+                            dayName = "Not number day of the week!";
+                            break;
+                    }
+
+                    Console.WriteLine($"This is {dayName}");
+                }
+                else
+                {
+                    if (str != "EXIT")
+                    { 
+                      Console.WriteLine("Please, enter number!");
+                    }
+                }
+
+
+            } while (str != "EXIT") ;
+         
+          
+          
+        }
+    }
+ }
---
+++ Solution/Multiplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application return multiplication table by number, whitch you enter");
+            string stringValue;
+            do
+            {
+                int number = 0;
+                Console.WriteLine("Please, enter number or 'EXIT' for exit application!");
+                stringValue = Console.ReadLine();
+
+                bool isInt = int.TryParse(stringValue, out number);
+
+                if (isInt)
+                {
+                    int result = 0;
+                    for (int i = 0; i <= 10; i++)
+                    {
+                        result = number * i;
+                        Console.WriteLine($"{number} X {i} = {result}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please, enter a number");
+                }
+
+            } while (stringValue != "EXIT");
+        }
+    }
+}
