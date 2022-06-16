d02ea3b50315f320566cb00933a747398b05ee62
Aliaksandr <mosya911@gmail.com>
2022-06-16 21:36:55 +0300

    temp Cycles

---
+++ Solution/TempCode.Cycles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TempCode.Cycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int defaultHeight = 1;
+
+            Console.WriteLine("Add height of the triangle");
+
+            var isInt = int.TryParse(Console.ReadLine(),out defaultHeight);
+
+            if (isInt)
+            {
+                for (int i = 0; i < defaultHeight; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+            else
+                Console.WriteLine("Enter a number");
+            
+
+        }
+    }
+}
