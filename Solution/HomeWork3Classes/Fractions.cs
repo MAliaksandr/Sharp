518315407aa9f7225df28a5d4708b717a63e01fd
Aliaksandr <mosya911@gmail.com>
2022-06-22 10:02:38 +0300

    Add Class Fractions

---
+++ Solution/HomeWork3Classes/Fractions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3Classes
+{
+    public class Fractions
+    {
+        private long wholePart;
+        private ushort fracPart;
+
+        public long WholePart
+        {
+            get { return wholePart; }
+        }
+
+        public ushort FracPart
+        {
+            get { return fracPart; }
+        }
+
+        public Fractions()
+        {
+            wholePart = 0;
+            fracPart = 0;
+        }
+
+        public double GetFractions(double dblValue)
+        {
+            var wholeValue = (int)dblValue;
+
+            var dblValueToStr = dblValue.ToString();
+
+            string stringValue = dblValueToStr.Substring(dblValueToStr.IndexOf(',') + 1);
+
+            ushort strFracToInt = Convert.ToUInt16(stringValue);
+
+            this.wholePart = wholeValue;
+
+            this.fracPart = strFracToInt;
+
+            return dblValue;
+       }
+
+        public static void ReturnSum(double firstValue, double secondValue)
+        {
+            var result = firstValue + secondValue;
+
+            Console.WriteLine($"Sum = {result}");
+        }
+
+        public static void ReturnSubtraction(double firstValue, double secondValue)
+        {
+            double result = firstValue - secondValue;
+
+            Console.WriteLine($"Result of Subtraction = {result}");
+        }
+
+        public static void ReturnMultiplic(double firstValue, double secondValue)
+        {
+            var result = firstValue * secondValue;
+
+            Console.WriteLine($"Result of multiplication = {result}");
+        }
+
+        public static void ReturnСompare(double firstValue, double secondValue)
+        {
+            if (firstValue > secondValue)
+            {
+                Console.WriteLine($"firstValue: {firstValue} > secondValue:{secondValue}");
+            }
+            else if (firstValue < secondValue)
+            {
+                Console.WriteLine($"secondValue: {firstValue} > firstValue:{secondValue}");
+            }
+            else
+            {
+                Console.WriteLine($"firstValue = secondValue = {firstValue} ");
+            }
+        }
+
+    }
+}
+
---
+++ Solution/HomeWork3Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork3Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var element_1 = new Fractions();
+            var element_2 = new Fractions();
+
+            var firstValue = element_1.GetFractions(12.079);
+
+            var secondValue = element_2.GetFractions(12.079);
+
+            Console.WriteLine($"elem1 = { firstValue} \t & elem2 = {secondValue}");
+
+            var wholePart = element_1.WholePart;
+
+            Console.WriteLine($"WholePart  = {wholePart} and FracPart ={element_1.FracPart}");
+
+            Fractions.ReturnSum(firstValue, secondValue);
+
+            Fractions.ReturnSubtraction(firstValue, secondValue);
+
+            Fractions.ReturnMultiplic(firstValue, secondValue);
+
+            Fractions.ReturnСompare(firstValue, secondValue);
+       }
+    }
+}
