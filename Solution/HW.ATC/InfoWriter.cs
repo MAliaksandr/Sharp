b7662d6c9bf43146477526a68eb4520be7094816
Aliaksandr <mosya911@gmail.com>
2022-07-29 22:21:39 +0300

    InfoWriter realisation

--- Solution/HW.ATC/InfoWriter.cs
+++ Solution/HW.ATC/InfoWriter.cs
@@ -9,5 +9,26 @@ namespace HW.ATC
     // пишем в файл
     internal class InfoWriter
     {
+       public void RecordInfo(string info)
+        {
+            if (info != null)
+            { 
+                try
+                {
+                    using (var sw = new StreamWriter(LogWriter.FilePath, true))
+                    {
+                        sw.WriteLine($"{info}");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine($"Cann't write info: {info} into file {ex.ToString}"); 
+                }
+            }
+        }
+
+
     }
 }
